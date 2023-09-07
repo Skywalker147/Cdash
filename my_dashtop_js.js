4b26b3f5b9f594f0e76b2d00182111a9dd37b033
Skywalker147 <144387956+Skywalker147@users.noreply.github.com>
2023-09-08 02:02:12 +0530

    added my_dashtop_js.js

---
+++ my_dashtop_js.js
@@ -0,0 +1,10 @@
+
+function button1()
+{
+    window.open( "https://www.coursera.org/learn/ai-for-everyone?action=enroll&utm_campaign=WebsiteCoursesAI4ETopButton&utm_medium=institutions&utm_source=deeplearningai", '_blank');
+}
+
+function button2()
+{
+    window.open( "https://www.deeplearning.ai/short-courses/chatgpt-prompt-engineering-for-developers/", '_blank');
+}
